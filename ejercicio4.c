0a5490d170a4d18cb98a56b67f7a6239dc4cbe7e
Claudia Agostinelli <cagostinelli@unsa.edu.pe>
2025-10-21 08:38:59 -0500

    ejercicio4

---
+++ ejercicio4.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define vertex int
+
+typedef struct node *link;
+struct node {
+    vertex w;
+    link next;
+};
+
+struct graph {
+    int V;
+    int A;
+    link *adj;
+};
+typedef struct graph *Graph;
+
+static link NEWnode(vertex w, link next) {
+    link a = malloc(sizeof(struct node));
+    a->w = w;
+    a->next = next;
+    return a;
+}
+
+Graph GRAPHinit(int V) {
+    Graph G = malloc(sizeof *G);
+    G->V = V;
+    G->A = 0;
+    G->adj = malloc(V * sizeof(link));
+    for (vertex v = 0; v < V; ++v)
+        G->adj[v] = NULL;
+    return G;
+}
+
+void GRAPHinsertArc(Graph G, vertex v, vertex w) {
+    for (link a = G->adj[v]; a != NULL; a = a->next)
+        if (a->w == w) return;
+    G->adj[v] = NEWnode(w, G->adj[v]);
+    G->A++;
+}
+
+void GRAPHshow(Graph G) {
+    for (vertex v = 0; v < G->V; v++) {
+        printf("%d:", v);
+        for (link a = G->adj[v]; a != NULL; a = a->next) {
+            printf(" %d", a->w);
+        }
+        printf("\n");
+    }
+}
+
+// Ejemplo para probar
+int main() {
+    Graph G = GRAPHinit(6);
+
+    GRAPHinsertArc(G, 0, 1);
+    GRAPHinsertArc(G, 0, 5);
+    GRAPHinsertArc(G, 1, 0);
+    GRAPHinsertArc(G, 1, 5);
+    GRAPHinsertArc(G, 2, 4);
+    GRAPHinsertArc(G, 3, 1);
+    GRAPHinsertArc(G, 5, 3);
+
+    printf("Mostrar grafo:\n");
+    GRAPHshow(G);
+
+    return 0;
+}
